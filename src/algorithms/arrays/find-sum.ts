8bf5c24ba8fe5f165a7da0d27ee8d7a0ad442167
Saidjamol <saidjamol1@gmail.com>
2025-04-03 09:30:50 +0500

    feat(arrays): implement and test find two numbers algorithm

---
+++ src/algorithms/arrays/find-sum.ts
@@ -0,0 +1,44 @@
+/**
+ * Given an array of integers, nums, and an integer target, k,
+ * find two numbers in the array that sum up to the target k.
+ *
+ * Time complexity O(n) - where n is the number of elements in the array
+ * Space complecity (On) - where n is the size of the Map
+ *
+ * @param {number[]} nums - The input array of numbers;
+ * @param {number} k - The target number that two elements in the nums array sum up to
+ * @returns {number[]} - A new array containing elements sum up to the target k parameter
+ *
+ * @constraints
+ * 2 ≤ nums.length ≤ 10^3
+ * -10^5 ≤ nums[i] ≤ 10^5
+ * -10^5 ≤ k ≤ 10^5
+ *
+ * @example
+ * findSum([2,4,6,8,10,19], 21) // returns [2, 19]
+ * findSum([-4,-8,0,-7,-3,-10] , -15) // returns [-8,-7]
+ *
+ */
+
+export function findSum(nums: number[], k: number): number[] {
+    if (isNaN(k) || !Array.isArray(nums)) {
+        throw new TypeError(
+            "nums must be valid array of integers and k must be a valid number",
+        ) as never;
+    }
+
+    const seen: Map<number, number> = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const current = nums[i];
+        const complement = k - current;
+
+        if (seen.has(complement)) {
+            return [complement, current];
+        }
+
+        seen.set(current, i);
+    }
+
+    return [];
+}
---
+++ tests/algorithms/arrays/find-sum.test.ts
@@ -0,0 +1,43 @@
+import { findSum } from '../../../src/algorithms/arrays/find-sum';
+
+describe('findSum', () => {
+    test('should find the sum of positive two numbers in the array equal to given 2nd parameter', () => {
+        const result = findSum([2,4,6,8,10,19], 21);
+
+        expect(result.length).toBe(2);
+        expect(result).toContain(2);
+        expect(result).toContain(19);
+    });
+
+    test('should find the sum of negative two numbers in the array equal to given 2nd parameter', () => {
+        const result = findSum([-4,-8,0,-7,-3,-10], -15);
+
+        expect(result.length).toBe(2);
+        expect(result).toContain(-7);
+        expect(result).toContain(-8);
+    });
+
+    test('should find the sum two numbers in the longer array equal to given 2nd parameter', () => {
+        const result = findSum([-1,9,56,12,-13,-6,23,19,71,-56,-14], -44);
+
+        expect(result.length).toBe(2);
+        expect(result).toContain(-56);
+        expect(result).toContain(12);
+    });
+
+    test('should find sum in the short array', () => {
+        const result = findSum([3,3], 6);
+        
+        expect(result.length).toBe(2);
+        expect(result).toContain(3);
+        expect(result).toContain(3);
+    });
+
+    test('should find the sum of two number in the array equal to 0', () => {
+        const result = findSum([49,17,15,22,-45,29,18,-15,11,37,12,-52], 0);
+
+        expect(result.length).toBe(2);
+        expect(result).toContain(15);
+        expect(result).toContain(-15);
+    })
+});
