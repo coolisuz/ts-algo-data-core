3668434379221773196e37c67dce2d86399326f4
Saidjamol <saidjamol1@gmail.com>
2025-04-05 07:47:39 +0500

    feat(arrays) implement rearrange-sorted on O(1) space cplx

--- src/algorithms/arrays/rearrange-sorted.ts
+++ src/algorithms/arrays/rearrange-sorted.ts
@@ -18,10 +18,37 @@
  */
 
 /**
- *  This Two-Pointer runs O(n) in both time and space complexities
+ *  This Two-Pointer approach runs O(n) in both time and space complexities
  *  easy to read and implement
  */
 
+// export function rearrangeSorted(arr: number[]): number[] {
+//     if (!Array.isArray(arr)) {
+//         throw new TypeError("An array of numbers required") as never;
+//     }
+
+//     if (arr.length === 1) return arr;
+
+//     let result = [],
+//         left = 0,
+//         right = arr.length - 1
+
+//     while (left <= right) {
+//         result.push(arr[right]);
+
+//         if (left !== right) result.push(arr[left]);
+
+//         left++;
+//         right--;
+//     }
+
+//     return result;
+// }
+
+/**
+ *  This advanced Encoding&Decoding aprroach runs O(n) in time complexity but constant O(1) in space compexity
+ *  generally preffered in tight space envs.
+ */
 export function rearrangeSorted(arr: number[]): number[] {
     if (!Array.isArray(arr)) {
         throw new TypeError("An array of numbers required") as never;
@@ -29,20 +56,26 @@ export function rearrangeSorted(arr: number[]): number[] {
 
     if (arr.length === 1) return arr;
 
-    let result = [],
-        left = 0,
-        right = arr.length - 1
+    const maxEl = arr[arr.length - 1] + 1;
+    let left = 0;
+    let right = arr.length - 1;
 
-    while (left <= right) {
-        result.push(arr[right]);
-
-        if (left !== right) result.push(arr[left]);
-
-        left++;
-        right--;
+    // Encoding Phase
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 === 0) {
+            arr[i] += (arr[right] % maxEl) * maxEl;
+            right--;
+        } else {
+            arr[i] += (arr[left] % maxEl) * maxEl;
+            left++;
+        }
     }
 
-    return result;
-}
+    // Decoding Phase
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = Math.floor(arr[i] / maxEl);
+    }
 
+    return arr;
+}
 
