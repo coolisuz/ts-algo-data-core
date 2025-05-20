07ded2dac0be146a978c011663e80e226b7604cb
Saidjamol <saidjamol1@gmail.com>
2025-05-20 16:04:47 +0500

    feat(hash): initiate hash table constructor

---
+++ src/data-structures/hash/hash-table.ts
@@ -0,0 +1,21 @@
+import { HashEntry } from './hash-entry';
+
+/**
+ * A generic hash table implementation with separate chaining for collision resolution
+ * @template T The type of data stored in the hash table
+ */
+export class HashTable<T> {
+    private readonly slots: number;
+    private size: number;
+    private bucket: Array<HashEntry<T> | null>;
+
+    constructor(slots: number = 10) {
+        this.slots = slots;
+        this.size = 0;
+        this.bucket = [];
+
+        for (var i=0; i<this.slots; i++){
+            this.bucket[i] = null;
+          }
+    }
+}
--- src/data-structures/hash/index.ts
+++ src/data-structures/hash/index.ts
@@ -1,1 +1,2 @@
 export * from './hash-entry';
+export * from './hash-table';
