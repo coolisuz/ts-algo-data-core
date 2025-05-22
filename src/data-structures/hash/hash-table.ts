6435d51042754292d30360ce9e1162f1d226bd0f
Saidjamol <saidjamol1@gmail.com>
2025-05-22 18:57:35 +0500

    feat(hash): implement resize()

--- src/data-structures/hash/hash-table.ts
+++ src/data-structures/hash/hash-table.ts
@@ -5,7 +5,7 @@ import { HashEntry } from "./hash-entry";
  * @template T The type of data stored in the hash table
  */
 export class HashTable<T> {
-    private readonly slots: number;
+    private slots: number;
     private size: number;
     private bucket: Array<HashEntry<T> | null>;
 
@@ -31,5 +31,44 @@ export class HashTable<T> {
         let index = key % this.slots;
 
         return index;
+    }
+    
+    resize(): void {
+        const oldBucket = this.bucket;
+        const oldSlots = this.slots;
+
+        this.slots = oldSlots * 2;
+        this.size = 0;
+
+        this.bucket = [];
+        for (var i = 0; i < this.slots; i++) {
+            this.bucket[i] = null;
+        }
+
+        for (var i = 0; i < oldSlots; i++) {
+            let current = oldBucket[i];
+            
+            while (current !== null) {
+                const next = current.next;
+                current.next = null;
+                const newIndex = this.getIndex(current.key);
+                
+                if (this.bucket[newIndex] === null) {
+                    this.bucket[newIndex] = current;
+                } else {
+                    let chainCurrent = this.bucket[newIndex];
+                    while (chainCurrent !== null && chainCurrent.next !== null) {
+                        chainCurrent = chainCurrent.next;
+                    }
+
+                    if (chainCurrent !== null) {
+                        chainCurrent.next = current;
+                    }
+                }
+
+                this.size++;
+                current = next;
+            }
+        }
     }
 }
